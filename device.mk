6f57de822edecb1088f5e405775d6514a77085c7
h8rift <h8rift@gmail.com>
2014-02-19 20:51:20 -0600

    m4: add low_latency audio permission
    Change-Id: I3ef8fdbfaf53af35bcba0d2245be451b01bc7090

--- device.mk
+++ device.mk
@@ -96,6 +96,7 @@ PRODUCT_PACKAGES += \
 
 # Permissions
 PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
     frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml
 
