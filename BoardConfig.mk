688930aeac34e9eacfdd4a0e2705ad60dac46b30
Jurica Vukadin <u.ra604@gmail.com>
2014-09-15 18:14:24 +0200

    m4: Fix network search
    Change-Id: I8cb6a7c8ffd13a5564e60afbefe0b0a81caa1466

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -72,6 +72,7 @@ TARGET_POWERHAL_VARIANT := cm
 
 # RIL
 BOARD_PROVIDES_LIBRIL := true
+BOARD_RIL_FIVE_SEARCH_RESPONSES := true
 
 # USB
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
