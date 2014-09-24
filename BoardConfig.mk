94f7c24f4a94a72bbacbf3c2b71ecac507afc0ed
Jurica Vukadin <u.ra604@gmail.com>
2014-09-25 00:08:39 +0200

    m4: Fix cmhw path
    Change-Id: I207b21224422c7f61b7feb98bcc076dd1f333d2f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -75,7 +75,7 @@ BOARD_PROVIDES_LIBRIL := true
 BOARD_RIL_FIVE_SEARCH_RESPONSES := true
 
 # Tuning
-BOARD_HARDWARE_CLASS := device/htc/m7-common/cmhw
+BOARD_HARDWARE_CLASS := device/htc/m4/cmhw
 
 # USB
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
