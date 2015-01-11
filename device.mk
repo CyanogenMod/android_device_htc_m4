a157417836bea9ce80e8d9866b70545142499279
Ethan Chen <intervigil@gmail.com>
2015-01-11 14:24:05 -0800

    m4: Remove deprecated torch setup
    Change-Id: I4873ad3faafee1a65dd5e8606bd2aec6b5ca35a7

--- device.mk
+++ device.mk
@@ -92,10 +92,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermald.conf:system/etc/thermald.conf
 
-# Torch
-PRODUCT_PACKAGES += \
-    Torch
-
 # WiFi
 PRODUCT_PACKAGES += \
     libnetcmdiface
