7ecc66f2a46022bfb183d7768ecd9df0d1449e24
Brinly Taylor <uberlaggydarwin@gmail.com>
2015-02-22 13:27:52 +1030

    m4: enable OTG storage settings
    Change-Id: I3210acdcc222e0baf69f0de8d29d5b49057d2fc6

--- system_prop.mk
+++ system_prop.mk
@@ -26,3 +26,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Time services
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.timed.enable=true
+
+# Misc
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.isUsbOtgEnabled=true
